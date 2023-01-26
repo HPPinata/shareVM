151d617f9248665564a35a33892cb8a9a231d65c
HPPinata <83947761+HPPinata@users.noreply.github.com>
2023-01-26 18:48:16 +0100

    formatting and remove useless share

--- combustion.bash
+++ combustion.bash
@@ -14,20 +14,20 @@ mount /dev/sr1 /mnt
 zypper rm -yu xen-tools-domU
 /mnt/Linux/install.sh -d sles -m 15 -n
 
-lsblk
 wipefs -f -a /dev/xvdd /dev/xvde
-sleep 5
+sleep 1
 parted -s -a optimal /dev/xvdd 'mklabel gpt mkpart primary 0% 100%'
 parted -s -a optimal /dev/xvde 'mklabel gpt mkpart primary 0% 100%'
-sleep 5
+sleep 1
 
 mkfs.btrfs -f -L data -m raid1 -d raid1 /dev/xvdd1 /dev/xvde1
+
 mkdir -p /var/nfsshare
 chown -R root:root /var/nfsshare
 chmod -R 777 /var/nfsshare
+
 { echo; echo '/dev/xvdd1  /var/nfsshare  btrfs  nofail  0  2'; } >> /etc/fstab
 cat /etc/fstab
 
-mkdir -p /var/nfsshare/vm /var/nfsshare/nc
+mkdir -p /var/nfsshare/vm
 echo '/var/nfsshare/vm  *(rw)' >> /etc/exports
-echo '/var/nfsshare/nc  *(rw)' >> /etc/exports

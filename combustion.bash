0d76469082e42073ce4648133bf37f4a0420a1d3
HPPinata <83947761+HPPinata@users.noreply.github.com>
2023-01-26 23:32:22 +0100

    correct path

--- combustion.bash
+++ combustion.bash
@@ -21,7 +21,7 @@ parted -s -a optimal /dev/xvde 'mklabel gpt mkpart primary 0% 100%'
 sleep 1
 
 mkfs.btrfs -f -L data -m raid1 -d raid1 /dev/xvdd1 /dev/xvde1
-mkdir -p /var/nfsshare/vm/sr
+mkdir -p /var/nfsshare
 mount /dev/xvdd1 /var/nfsshare
 
 { echo; echo '/dev/xvdd1  /var/nfsshare  btrfs  nofail  0  2'; } >> /etc/fstab
@@ -31,4 +31,3 @@ mkdir -p /var/nfsshare/vm
 echo '/var/nfsshare/vm  *(rw)' >> /etc/exports
 chown -R root:root /var/nfsshare
 chmod -R 777 /var/nfsshare
-

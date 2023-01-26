460ac1a6e48509e777700e7c87b3861c2bf11c2f
HPPinata <83947761+HPPinata@users.noreply.github.com>
2023-01-26 18:11:12 +0100

    Create combustion.bash

---
+++ combustion.bash
@@ -0,0 +1,33 @@
+#!/bin/bash
+# combustion: network
+
+echo 'root:HASHchangeME' | chpasswd -e
+echo 'nfsshare' > /etc/hostname
+
+zypper in -y nfs-kernel-server
+systemctl enable nfs-server
+mount /dev/xvda4 /var
+
+echo 'PermitRootLogin yes' > /etc/ssh/sshd_config.d/root.conf
+
+mount /dev/sr1 /mnt
+zypper rm -yu xen-tools-domU
+/mnt/Linux/install.sh -d sles -m 15 -n
+
+lsblk
+wipefs -f -a /dev/xvdd /dev/xvde
+sleep 5
+parted -s -a optimal /dev/xvdd 'mklabel gpt mkpart primary 0% 100%'
+parted -s -a optimal /dev/xvde 'mklabel gpt mkpart primary 0% 100%'
+sleep 5
+
+mkfs.btrfs -f -L data -m raid1 -d raid1 /dev/xvdd1 /dev/xvde1
+mkdir -p /var/nfsshare
+chown -R root:root /var/nfsshare
+chmod -R 777 /var/nfsshare
+{ echo; echo '/dev/xvdd1  /var/nfsshare  btrfs  nofail  0  2'; } >> /etc/fstab
+cat /etc/fstab
+
+mkdir -p /var/nfsshare/vm /var/nfsshare/nc
+echo '/var/nfsshare/vm  *(rw)' >> /etc/exports
+echo '/var/nfsshare/nc  *(rw)' >> /etc/exports

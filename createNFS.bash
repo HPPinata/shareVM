f92c032cd50a75e393283df59ec3c777d342c6c9
HPPinata <83947761+HPPinata@users.noreply.github.com>
2023-03-29 15:27:52 +0200

    VBD start index

--- createNFS.bash
+++ createNFS.bash
@@ -47,7 +47,7 @@ create-VM () {
   xe vdi-param-set uuid=$vdiUID name-label=nfsshare
   
   passUID=$(xe vdi-list sr-uuid=$passSR | grep -e uuid | grep -v sr | awk -F ': ' {'print $2'})
-  N=3
+  N=4
   delim=""
   joined=""
   prefix="[NOBAK]"

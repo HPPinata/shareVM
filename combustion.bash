73110874a96e0b74f161c7fbd61ac07f79b9fe55
HPPinata <83947761+HPPinata@users.noreply.github.com>
2023-04-22 21:01:11 +0200

    Update drive names

--- combustion.bash
+++ combustion.bash
@@ -5,11 +5,7 @@ echo 'root:HASHchangeME' | chpasswd -e
 echo 'nfsshare' > /etc/hostname
 echo 'PermitRootLogin yes' > /etc/ssh/sshd_config.d/root.conf
 
-mount /dev/xvda4 /var
-mount /dev/sr1 /mnt
-
-zypper rm -yu xen-tools-domU
-/mnt/Linux/install.sh -d sles -m 15 -n
+mount /dev/vda4 /var
 
 zypper in -y nfs-kernel-server zram-generator
 systemctl enable nfs-server
@@ -21,15 +17,13 @@ zram-size = ram
 compression-algorithm = zstd
 EOL
 
-sed -i "s+SELINUX=enforcing+SELINUX=permissive+g" /etc/selinux/config
-
-wipefs -f -a /dev/xvde /dev/xvdf
-mkfs.btrfs -f -L data -m raid1 -d raid1 /dev/xvde /dev/xvdf
+wipefs -f -a /dev/sda /dev/sdb
+mkfs.btrfs -f -L data -m raid1 -d raid1 /dev/sda /dev/sdb
 
 mkdir -p /var/nfsshare/mnt
 mount /dev/xvde /var/nfsshare/mnt
 
-{ echo; echo '/dev/xvde  /var/nfsshare/mnt  btrfs  nofail  0  2'; } >> /etc/fstab
+{ echo; echo '/dev/sda  /var/nfsshare/mnt  btrfs  nofail  0  2'; } >> /etc/fstab
 cat /etc/fstab
 
 mkdir /var/nfsshare/mnt/xen

74fd1fa0c34f0228722b11a2f167d7c0d92e8edc
HPPinata <83947761+HPPinata@users.noreply.github.com>
2023-04-23 15:48:27 +0200

    update directories & drive name

--- combustion.bash
+++ combustion.bash
@@ -21,16 +21,13 @@ wipefs -f -a /dev/sda /dev/sdb
 mkfs.btrfs -f -L data -m raid1 -d raid1 /dev/sda /dev/sdb
 
 mkdir -p /var/nfsshare/mnt
-mount /dev/xvde /var/nfsshare/mnt
+mount /dev/sda /var/nfsshare/mnt
 
 { echo; echo '/dev/sda  /var/nfsshare/mnt  btrfs  nofail  0  2'; } >> /etc/fstab
 cat /etc/fstab
 
-mkdir /var/nfsshare/mnt/xen
+mkdir /var/nfsshare/mnt/vms
 mkdir /var/nfsshare/mnt/net
 
-echo '/var/nfsshare/mnt/xen  *(rw,no_root_squash)' >> /etc/exports
+echo '/var/nfsshare/mnt/vms  *(rw,no_root_squash)' >> /etc/exports
 echo '/var/nfsshare/mnt/net  *(rw,no_root_squash)' >> /etc/exports
-
-mkdir /var/nfsshare/mnt/xen/sr
-mkdir /var/nfsshare/mnt/xen/iso

830e40276a525fa5390670a8a6785751f658a44e
HPPinata <83947761+HPPinata@users.noreply.github.com>
2023-04-28 15:26:10 +0200

    use bcache

--- combustion.bash
+++ combustion.bash
@@ -7,7 +7,7 @@ echo 'PermitRootLogin yes' > /etc/ssh/sshd_config.d/root.conf
 
 mount /dev/vda4 /var
 
-zypper in -y cron duperemove nfs-kernel-server zram-generator
+zypper in -y bcache-tools cron duperemove nfs-kernel-server zram-generator
 systemctl enable nfs-server
 
 cat <<'EOL' > /etc/systemd/zram-generator.conf
@@ -17,13 +17,20 @@ zram-size = ram
 compression-algorithm = zstd
 EOL
 
-wipefs -f -a /dev/sda /dev/sdb
-mkfs.btrfs -f -L data -m raid1 -d raid1 /dev/sda /dev/sdb
+echo 1 | tee /sys/fs/bcache/*/stop
+echo 1 | tee /sys/block/bcache*/bcache/stop
+sleep 1
+
+wipefs -f -a /dev/sda /dev/sdb /dev/vdb
+make-bcache -B /dev/sda /dev/sdb -C /dev/vdb
+wipefs -f -a /dev/bcache0 /dev/bcache1
+
+mkfs.btrfs -f -L data -m raid1 -d raid1 /dev/bcache0 /dev/bcache1
 
 mkdir -p /var/nfsshare/mnt
-mount /dev/sda /var/nfsshare/mnt
+mount /dev/bcache0 /var/nfsshare/mnt
 
-{ echo; echo '/dev/sda  /var/nfsshare/mnt  btrfs  nofail  0  2'; } >> /etc/fstab
+{ echo; echo '/dev/bcache0  /var/nfsshare/mnt  btrfs  nofail  0  2'; } >> /etc/fstab
 cat /etc/fstab
 
 mkdir /var/nfsshare/mnt/vms

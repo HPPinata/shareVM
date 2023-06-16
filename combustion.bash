accc09ac8e5b4179c734d43d31cc4cc04fa60334
HPPinata <83947761+HPPinata@users.noreply.github.com>
2023-06-16 16:56:11 +0200

    use bcache, samba, snapper
    reference proxmox branch, this is identical apart from drive names and guest tools

--- combustion.bash
+++ combustion.bash
@@ -2,7 +2,7 @@
 # combustion: network
 
 echo 'root:HASHchangeME' | chpasswd -e
-echo 'nfsshare' > /etc/hostname
+echo 'netshare' > /etc/hostname
 echo 'PermitRootLogin yes' > /etc/ssh/sshd_config.d/root.conf
 
 mount /dev/xvda4 /var
@@ -11,7 +11,10 @@ mount /dev/sr1 /mnt
 zypper rm -yu xen-tools-domU
 /mnt/Linux/install.sh -d sles -m 15 -n
 
-zypper in -y nfs-kernel-server zram-generator
+zypper in -y bcache-tools cron duperemove nfs-kernel-server \
+policycoreutils-python-utils samba snapper zram-generator
+
+systemctl enable smb
 systemctl enable nfs-server
 
 cat <<'EOL' > /etc/systemd/zram-generator.conf
@@ -23,20 +26,82 @@ EOL
 
 sed -i "s+SELINUX=enforcing+SELINUX=permissive+g" /etc/selinux/config
 
-wipefs -f -a /dev/xvde /dev/xvdf
-mkfs.btrfs -f -L data -m raid1 -d raid1 /dev/xvde /dev/xvdf
+modprobe bcache
+echo 1 | tee /sys/fs/bcache/*/stop
+echo 1 | tee /sys/block/bcache*/bcache/stop
+sleep 1
 
-mkdir -p /var/nfsshare/mnt
-mount /dev/xvde /var/nfsshare/mnt
+pass=$(ls -I xvda xvdd /dev/xvd*)
+wipefs -f -a ${pass[@]} /dev/xvdd
 
-{ echo; echo '/dev/xvde  /var/nfsshare/mnt  btrfs  nofail  0  2'; } >> /etc/fstab
-cat /etc/fstab
+bcache make -C /dev/xvdd
+bcache register /dev/xvdd
+sleep 1
 
-mkdir /var/nfsshare/mnt/xen
-mkdir /var/nfsshare/mnt/net
+for blk in ${pass[@]}; do
+  bcache make -B $blk
+  bcache register $blk
+  sleep 1
+  bcache attach /dev/xvdd $blk
+  bcache set-cachemode $blk writeback
+done
 
-echo '/var/nfsshare/mnt/xen  *(rw,no_root_squash)' >> /etc/exports
-echo '/var/nfsshare/mnt/net  *(rw,no_root_squash)' >> /etc/exports
+wipefs -f -a $(ls /dev/bcache*)
+mkfs.btrfs -f -L data -m raid1 -d raid1 /dev/bcache*
 
-mkdir /var/nfsshare/mnt/xen/sr
-mkdir /var/nfsshare/mnt/xen/iso
+mkdir -p /var/share/mnt
+mount /dev/bcache0 /var/share/mnt
+
+{ echo; echo '/dev/bcache0  /var/share/mnt  btrfs  nofail  0  2'; } >> /etc/fstab
+
+mkdir /var/share/mnt/vms
+mkdir /var/share/mnt/net
+
+{ echo 'SMBchangeME'; echo 'SMBchangeME'; } | smbpasswd -a root
+pdbedit -u root --set-nt-hash 'SMBchangeME'
+
+cat <<'EOL' > /etc/samba/smb.conf
+[smb-net]
+    comment = user data network share
+    path = /var/share/mnt/net
+    read only = no
+    inherit owner = yes
+    inherit permissions = yes
+EOL
+semanage fcontext -at samba_share_t '/var/share/mnt/net(/.*)?'
+
+echo '/var/share/mnt/vms  xcp-ng(rw,async,no_root_squash)' >> /etc/exports
+
+mkdir /var/share/mnt/.duperemove
+
+cat <<'EOL' > /var/share/snapper.bash
+#!/bin/bash
+
+timedatectl set-timezone Europe/Berlin
+localectl set-keymap de
+
+snapper -c data create-config /var/share/mnt
+snapper -c data set-config "TIMELINE_CREATE=yes" "TIMELINE_CLEANUP=yes" \
+"TIMELINE_LIMIT_HOURLY=24" "TIMELINE_LIMIT_DAILY=7" "TIMELINE_LIMIT_WEEKLY=6" \
+"TIMELINE_LIMIT_MONTHLY=0" "TIMELINE_LIMIT_YEARLY=0"
+
+snapper -c data setup-quota
+semanage fcontext -at snapperd_data_t '/var/share/mnt/.snapshots(/.*)?'
+
+cat <<'EOF' | crontab -
+SHELL=/bin/bash
+BASH_ENV=/etc/profile
+
+@reboot restorecon -Rv /
+#@reboot echo 0 | tee /sys/block/bcache*/bcache/sequential_cutoff
+0 6 * * 1 duperemove -dhr -b 64K --dedupe-options=same --hash=xxhash --hashfile=/var/share/mnt/.duperemove/hashfile.db /var/share/mnt
+0 5 1 * * rm -rf /var/share/mnt/.duperemove/hashfile.db && btrfs filesystem defragment -r /var/share/mnt
+EOF
+EOL
+chmod +x /var/share/snapper.bash
+
+cat <<'EOL' | crontab -
+SHELL=/bin/bash
+BASH_ENV=/etc/profile
+@reboot bash /var/share/snapper.bash
+EOL
